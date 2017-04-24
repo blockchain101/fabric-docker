7337fc8fb73467607795f3058436c92feddf148f
RodgerZhuang <rodger0514@gmail.com>
2017-04-24 16:15:54 +0800

    updated to support car-leasing app using fabric 0.6.1

--- fabric-docker/dockerfiles/generate-baseimage.sh
+++ fabric-docker/dockerfiles/generate-baseimage.sh
@@ -1,2 +1,2 @@
 gzip -d github.com.tar.gz
-docker build -f Dockerfile_baseimage -t hyperledger/fabric-baseimage:latest .
+docker build -f Dockerfile_baseimage0.3.0 -t hyperledger/fabric-baseimage:latest .
